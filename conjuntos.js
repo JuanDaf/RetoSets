053ffdd1a5d51088799be220c38ac60ad1947cc4
juandaf <juandaforer@gmail.com>
2023-02-21 10:36:33 -0500

    Cargando archivos complementarios, conjuntos.js

---
+++ conjuntos.js
@@ -0,0 +1,11 @@
+const  array =["Andrea","Camilo","Andres","Carlos"]
+
+const set =  new Set(array)
+
+//console.log(array)
+console.log(set)
+
+set.add("Carlos")
+console.log(set)
+set.add("Javascript")
+console.log(set)
